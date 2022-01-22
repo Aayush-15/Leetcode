b0a51b2946688b2a4a9ac1a4bba208ca1717bde1
Aayush Kansara <60152643+Aayush-15@users.noreply.github.com>
2022-01-22 15:10:19 +0530

    Time: 36 ms (21.57%), Space: 16.5 MB (24.92%) - LeetHub

---
+++ 877-stone-game/877-stone-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int helper(int l,int r,vector<vector<int>>&dp,vector<int>&piles)
+    {
+        if(l == r-1) return max(piles[l], piles[r]);
+        if(dp[l][r]!=-1)
+        {
+            return dp[l][r];
+        }
+        int ifleft=piles[l]+max(helper(l+2,r,dp,piles),helper(l+1,r-1,dp,piles));
+        int ifright=piles[r]+max(helper(l,r-2,dp,piles),helper(l+1,r-1,dp,piles));
+        return dp[l][r]=max(ifleft,ifright);
+    }
+    bool stoneGame(vector<int>& piles) 
+    {
+        int sz=piles.size();
+        int l=0;
+        int r=sz-1;
+        vector<vector<int>>dp(sz,vector<int>(sz,-1));
+        int alice=helper(l,r,dp,piles);
+        int total_sum=0;
+        for(auto x:piles)
+        {
+            total_sum+=(x);
+        }
+        int bob=total_sum-alice;
+        return alice>bob;
+    }
+};
