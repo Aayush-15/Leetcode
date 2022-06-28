538efa6c8fa5c57eccf99f0d0bafc0d19a617291
Aayush Kansara <60152643+Aayush-15@users.noreply.github.com>
2022-06-28 08:34:04 +0530

    Time: 529 ms (59.27%), Space: 14.6 MB (62.10%) - LeetHub

--- 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
+++ 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
@@ -1,65 +1,86 @@
 class Solution {
-    int timer = 0;
-    int dfs(int node, int p, vector<vector<int>> &adj, vector<int> &par, vector<int> &XOR, vector<int> &nums, vector<int> &intime, vector<int> &outtime)
-    {
-        par[node] = p;
-        intime[node] = timer++;
-        int val = nums[node];
-        for (int adjNode : adj[node])
-        {
-            if (adjNode == p) continue;
-            val ^= dfs(adjNode, node, adj, par, XOR, nums, intime, outtime);
-        }
-        outtime[node] = timer++;
-        return XOR[node] = val;
-    }
 public:
-    int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
-        int n = nums.size(), res = INT_MAX;
-        vector<vector<int>> adj(n);
-        for (vector<int> &v : edges)
-        {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
-        }
-        vector<int> par(n, -1), XOR(n), intime(n), outtime(n);
-        dfs(0, -1, adj, par, XOR, nums, intime, outtime);
-        for (vector<int> &v : edges)
-        {
-            if (par[v[1]] != v[0])
-                swap(v[1], v[0]);
-        }
-        for (int i = 0; i < n - 1; i++)
-        {
-            for (int j = i + 1; j < n - 1; j++)
-            {
-                int n1 = edges[i][1], n2 = edges[j][1], n3 = 0;
-                // n1 is in subtree of n2
-                if (intime[n2] < intime[n1] && outtime[n1] < outtime[n2])
-                {
-                    int a = XOR[n1];
-                    int b = (XOR[n2] ^ XOR[n1]);
-                    int c = (XOR[0] ^ XOR[n2]);
-                    res = min(res, max({a, b, c}) - min({a, b, c}));
-                }
-                // n2 is in subtree of n1
-                else if (intime[n1] < intime[n2] && outtime[n2] < outtime[n1])
-                {
-                    int a = XOR[n2];
-                    int b = (XOR[n1] ^ XOR[n2]);
-                    int c = (XOR[0] ^ XOR[n1]);
-                    res = min(res, max({a, b, c}) - min({a, b, c}));
-                }
-                // n1 & n2 are separate
-                else
-                {
-                    int a = XOR[n1];
-                    int b = XOR[n2];
-                    int c = (XOR[0] ^ XOR[n1] ^ XOR[n2]);
-                    res = min(res, max({a, b, c}) - min({a, b, c}));
-                }
-            }
-        }
-        return res;
-    }
+	int timer = 0;
+	void precalculate(vector<vector<int>>&adj, vector<int>&pre, int node, int par, vector<int>&nums, vector<int>&parent, vector<int>&inTime, vector<int>&outTime)
+	{
+		int curr_val = nums[node];
+		parent[node] = par;
+		inTime[node] = timer++;
+
+		for (auto x : adj[node])
+		{
+			if (x == par)
+			{
+				continue;
+			}
+			precalculate(adj, pre, x, node, nums, parent, inTime, outTime);
+			curr_val = curr_val ^ pre[x];
+		}
+
+		outTime[node] = timer++;
+
+		pre[node] = curr_val;
+	}
+	int minimumScore(vector<int>& nums, vector<vector<int>>& edges)
+	{
+		int nodes = nums.size();
+		vector<vector<int>>adj(nodes, vector<int>());
+		vector<int>pre(nodes, 0);//Storing Xor Values Tree Wise
+		vector<int>inTime(nodes, 0);
+		vector<int>outTime(nodes, 0);
+		vector<int>parent(nodes, -1);
+		for (auto x : edges)
+		{
+			int yy = x[0];
+			int zz = x[1];
+			adj[yy].push_back(zz);
+			adj[zz].push_back(yy);
+		}
+		precalculate(adj, pre, 0, -1, nums, parent, inTime, outTime);
+		for (auto &x : edges)
+		{
+			if (parent[x[1]] != x[0])
+			{
+				swap(x[0], x[1]);
+			}
+		}
+		int res= INT_MAX;
+		for (int i = 0; i < nodes - 1; i++)
+		{
+			for (int j = i + 1; j < nodes - 1; j++)
+			{
+				int n1 = edges[i][1];
+				int n2 = edges[j][1];
+				//n1 is in the subtree of n2
+				if (inTime[n2] < inTime[n1] && outTime[n1] < outTime[n2])
+				{
+					int a = pre[n1];
+					int b = (pre[n2] ^ pre[n1]);
+					int c = (pre[0] ^ pre[n2]);
+					res = min(res, max({a, b, c}) - min({a, b, c}));
+				}
+				// n2 is in subtree of n1
+				else if (inTime[n1] < inTime[n2] && outTime[n2] < outTime[n1])
+				{
+					int a = pre[n2];
+					int b = (pre[n1] ^ pre[n2]);
+					int c = (pre[0] ^ pre[n1]);
+					res = min(res, max({a, b, c}) - min({a, b, c}));
+				}
+				// n1 & n2 are separate
+				else
+				{
+					int a = pre[n1];
+					int b = pre[n2];
+					int c = (pre[0] ^ pre[n1] ^ pre[n2]);
+					res = min(res, max({a, b, c}) - min({a, b, c}));
+				}
+
+			}
+		}
+		// debug(res);
+		return res;
+		// debug(pre);
+		return 0;
+	}
 };
