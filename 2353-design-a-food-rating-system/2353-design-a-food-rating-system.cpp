353fea9984f1e8785f1f1d46189dc0f3aa9ac7b1
Aayush Kansara <60152643+Aayush-15@users.noreply.github.com>
2022-07-24 17:54:17 +0530

    Time: 1083 ms (33.33%), Space: 168.6 MB (33.33%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -2,14 +2,14 @@ struct Food{
   string Name;
   string Cuisine;
   int rating;
-  
+    
     Food(string name_,string cuis_,int rating_)
     {
         Name=name_;
         Cuisine=cuis_;
         rating=rating_;
     }
-    
+    //Custom Comperator
     friend bool operator<(const Food &x,const Food &y)
     {
         if(x.rating==y.rating)
@@ -19,37 +19,29 @@ struct Food{
         return x.rating>y.rating;
     }
 };
-
-
 class FoodRatings {
 public:
-    map<string,set<Food>>all_info;
-    map<string, pair<string, int>> cuisine_and_rating_per_food;//maps food To Cuisine
+    map<string,set<Food>>Food_Info;//Map Cuisine to Food
+    map<string, pair<string, int>> food_to_cuisine;//maps food To Cuisine
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) 
     {
        for(int i=0;i<foods.size();i++)
        {
-           cuisine_and_rating_per_food[foods[i]] = {cuisines[i], ratings[i]};
-           all_info[cuisines[i]].insert(Food(foods[i],cuisines[i],ratings[i]));
+           food_to_cuisine[foods[i]] = {cuisines[i], ratings[i]};
+           Food_Info[cuisines[i]].insert(Food(foods[i],cuisines[i],ratings[i]));
        }
     }
+    
     void changeRating(string food, int newRating) 
     {
-        int old_rating=cuisine_and_rating_per_food[food].second;
-        string cuisine_=cuisine_and_rating_per_food[food].first;
-        all_info[cuisine_].erase(Food(food,cuisine_,old_rating));
-        all_info[cuisine_].insert(Food(food,cuisine_,newRating));
-        cuisine_and_rating_per_food[food]={cuisine_,newRating};
+        int old_rating=food_to_cuisine[food].second;
+        string cuisine_=food_to_cuisine[food].first;
+        Food_Info[cuisine_].erase(Food(food,cuisine_,old_rating));
+        Food_Info[cuisine_].insert(Food(food,cuisine_,newRating));
+        food_to_cuisine[food]={cuisine_,newRating};
     }
     
     string highestRated(string cuisine) {
-        return all_info[cuisine].begin()->Name;
+        return Food_Info[cuisine].begin()->Name;
     }
 };
-
-/**
- * Your FoodRatings object will be instantiated and called as such:
- * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
- * obj->changeRating(food,newRating);
- * string param_2 = obj->highestRated(cuisine);
- */
